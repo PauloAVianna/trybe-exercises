6096cd3438bd6c2165b1041f4cd909f61f9b55f2
Paulo Vianna <p.avianna@hotmail.com>
2023-05-07 23:21:23 -0300

    learning_objects_manipulations

--- 1-fundamentals/3-introduction-to-JavaScript/day-04/object.js
+++ 1-fundamentals/3-introduction-to-JavaScript/day-04/object.js
@@ -6,7 +6,7 @@ let player = {
         golden: 2,
         silver: 3,
     }
-}
+};
 
 console.log('A jogadora ' + player.name + ' ' + player.lastName + ' tem ' + player.age + ' de idade.');
 
@@ -15,3 +15,33 @@ player.bestInTheWorld = [2006, 2007, 2008, 2009, 2010, 2018];
 console.log('A jogadora ' + player['name'] + ' ' + player['lastName'] + ' foi eleita a melhor do mundo por ' + player['bestInTheWorld'].length + ' vezes.');
 
 console.log('A jogadora possui ' + player.medals.golden + ' medalhas de ouro e ' + player.medals.silver + ' medalhas de prata.');
+
+let student = {};
+
+function addProperty(object, key, value) {
+    object[key] = value;
+};
+
+addProperty(student, 'name', 'Paulo');
+addProperty(student, 'email', 'p.avianna@hotmail.com');
+addProperty(student, 'telefone', 7599548470);
+addProperty(student, 'GitHub', 'PauloAVianna');
+addProperty(student, 'LinkedIn', 'Paulo André Vianna');
+console.log(student);
+
+let countries = {
+    franca: 'Paris',
+    brasil: 'Brasília',
+    espanha: 'Madrid',
+    portugal: 'Lisboa',
+  };
+
+let pairKeyValue = Object.entries(countries);
+
+console.log(pairKeyValue);
+
+for(index in pairKeyValue) {
+    console.log('--------');
+    console.log('País:', pairKeyValue[index][0]);
+    console.log('Capital:', pairKeyValue[index][1]);
+};
